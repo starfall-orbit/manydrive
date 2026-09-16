2ff9a1665e1149425ceaf9d46e8238baba5e49f7
contributory <bosuuap@alwaysdata.net>
2026-09-16 11:07:18 +0000

    fix: import storage selection check icon

--- app/src/main/kotlin/com/starfall/gsadrive/SystemFilesPage.kt
+++ app/src/main/kotlin/com/starfall/gsadrive/SystemFilesPage.kt
@@ -14,6 +14,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material.icons.outlined.Storage
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
